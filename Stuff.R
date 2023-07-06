a425b3141ec5c1cc7bc0131879250cc190438e40
Evan Wu <138053579+evanwu6@users.noreply.github.com>
2023-07-06 15:50:10 -0400

    Add files via upload

--- Stuff.R
+++ Stuff.R
@@ -2,20 +2,92 @@ library(tidyverse)
 library(readxl)
 library(stringi)
 library(patchwork)
+library(Lahman)
 
-# Function to remove accent marks from a string
+
+# Function to remove accent marks from a string ####
 remove_accents <- function(x) {
   stri_trans_general(x, "Latin-ASCII")
 }
 
+remove_pattern <- function(data, col, pattern) {
+  data %>%
+    mutate(!!col := gsub(pattern, "", !!sym(col)))
+}
+
+# New Function Cleaning ####
+separate_name_into_first_last <- function(data, full_name) {
+  data %>% 
+    separate(full_name, into = c("first_name", "last_name"), sep = " ")
+}
+rename_col <- function(data, first, last) {
+  data %>% 
+    rename(first_name = first, last_name = last)
+}
+combine_names <- function(data) {
+  data %>% mutate(full_name = paste(first_name, last_name, sep = " "))
+}
+clean_names <- function(data) {
+  remove_accents <- function(x) {
+    stri_trans_general(x, "Latin-ASCII")
+  }
+  
+  remove_pattern <- function(data, col, pattern) {
+    data %>%
+      mutate(!!col := gsub(pattern, "", !!sym(col)))
+  }
+  
+  # Remove accents
+  data <- data %>%
+    mutate_at(vars("first_name"), remove_accents)
+  
+  data <- data %>%
+    mutate_at(vars("last_name"), remove_accents)
+  
+  # Remove "."
+  data <- data %>% 
+    remove_pattern("first_name", "\\.")
+  
+  data <- data %>% 
+    remove_pattern("last_name", "\\.")
+  
+  data <- data %>% 
+    remove_pattern("last_name", "\\ Jr")
+  
+  data <- data %>% 
+    remove_pattern("first_name", "\\ ")
+  
+}
+
+# Ethan's Data ####
+e_stuff <- read_excel("Stuff Plus.xlsx", sheet = "Data") %>% 
+  separate_name_into_first_last("Name") %>% 
+  clean_names() %>% 
+  combine_names()
+
+e_movement <- read_excel("Stuff Plus.xlsx", sheet = "Movement") %>% 
+  filter(year > 2019) %>% 
+  clean_names() %>% 
+  combine_names()
+
+e_people_data <- Lahman::People %>%
+  rename_col("nameFirst", "nameLast") %>% 
+  select(first_name, last_name, playerID) %>% 
+  clean_names() %>% 
+  combine_names() %>% 
+  select(full_name, playerID) %>% 
+  rename("Name" = "full_name")
+
+# Importing Data ####
 stuff <- read_excel("Stuff Plus.xlsx", sheet = "Data")
 
 stuff_explore <- read_excel("Stuff Plus.xlsx", sheet = "Extended")
 
 movement_dirty <- read_excel("Stuff Plus.xlsx", sheet = "Movement")
-
 movement <- movement_dirty %>% 
   filter(year > 2019)
+
+pitches <- read_excel("Stuff Plus.xlsx", sheet = "Results_Pitch")
 
 # Reformatting Movement (Statcast) ####
 movement <- movement %>% 
@@ -38,25 +110,25 @@ movement <- movement %>%
 
 # Reformatting Stuff (Fangraphs) ####
 stuff <- stuff %>% 
-  rename(`Stf+ CUR` = `Stf+ CU`)
+  rename(`Stf CUR` = `Stf CU`)
 
 stuff <- stuff %>% 
-  mutate(`Stf+ CU` = case_when(
-    `Stf+ CUR` > -300 ~ `Stf+ CUR`,
-    `Stf+ KC` > -300 ~ `Stf+ KC`,
+  mutate(`Stf CU` = case_when(
+    `Stf CUR` > -300 ~ `Stf CUR`,
+    `Stf KC` > -300 ~ `Stf KC`,
     TRUE ~ NA))
 
 stuff <- stuff %>% 
-  select(-`Stf+ CUR`, -`Stf+ KC`)
+  select(-`Stf CUR`, -`Stf KC`)
 
 stuff <- stuff %>% 
-  rename(FF = `Stf+ FA`,
-         FC = `Stf+ FC`,
-         SI = `Stf+ SI`,
-         SL = `Stf+ SL`,
-         CU = `Stf+ CU`,
-         CH = `Stf+ CH`,
-         FS = `Stf+ FS`)
+  rename(FF = `Stf FA`,
+         FC = `Stf FC`,
+         SI = `Stf SI`,
+         SL = `Stf SL`,
+         CU = `Stf CU`,
+         CH = `Stf CH`,
+         FS = `Stf FS`)
 
 stuff <- stuff %>% 
   pivot_longer(c(FF:CH, CU))
@@ -84,9 +156,9 @@ stuff <- stuff %>%
 # Note: for fangraphs, knuckle and regular curve (priority) are integrated
 
 stuff_join <- stuff %>% 
-  select(IP:`Pitching+`, `Stf+ Pitch`, ID)
+  select(IP:`Pitching`, `Stf+ Pitch`, ID)
 
-# Aggregation
+# Aggregation ####
 data_init <- movement %>% 
   left_join(stuff_join, by = "ID")
 
@@ -97,7 +169,7 @@ data_init1 <- data_init %>%
 stuff_init1 <- stuff %>% 
   filter(Name == "Luis Garcia") %>%
   mutate(ID = paste0(Name, Season, Team, "_",pitch_type)) %>% 
-  select(IP:`Pitching+`, `Stf+ Pitch`, ID)
+  select(IP:`Pitching`, `Stf+ Pitch`, ID)
 
 movement_init1 <- movement %>%
   filter(Name == "Luis Garcia") %>%
@@ -107,13 +179,24 @@ data_init2 <- movement_init1 %>%
   left_join(stuff_init1, by = "ID")
 
 
-data <- rbind(
+data1 <- rbind(
   data_init1, data_init2)  
 
-data <- data %>% 
+data1 <- data1 %>% 
   select(Name, year:pitch_hand, pitch_type:pitch_type_name, 
          IP:`Stf+ Pitch`, pitcher_break_z:percent_rank_diff_x,
-         avg_speed:pitch_per)
+         avg_speed:pitch_per) %>% 
+  filter(!is.na(IP))
+
+data <- data1 %>% 
+  left_join(e_people_data, by = c("Name" = "Name"))
+
+data <- data %>% 
+  select(-`new name`) %>% 
+  filter(!is.na(IP)) %>% 
+  filter(!is.na(playerID)) %>% 
+  filter(Name != "Luis Garcia")
+
 
 # Graphs
 
@@ -236,3 +319,27 @@ data %>%
         panel.background = element_rect("white"),
         plot.title = element_text(hjust = 0.5)) + 
   coord_fixed(ratio = 1)
+
+# Exploratory Data Analysis of Pitch Results Data ####
+p_1 <- pitches %>% 
+  select(result:release_extension) %>% 
+  filter(pitches > 9)
+
+# Andrew Heaney FF Demo ####
+heaney <- p_1 %>% 
+  filter(player_name == "Heaney, Andrew") %>% 
+  select(result:pitches, player_name:pitch_percent, woba, launch_speed:velocity)
+
+heaney_fast <- heaney %>% 
+  filter(pitch_type == "FF")
+
+heaney_fast %>% 
+  ggplot(aes(x = result, y = woba, fill = pitches)) +
+  geom_col() +
+  scale_fill_continuous(low = "#FFE338", high = "forestgreen") +
+  labs(y = "wOBA",
+       x = "Result",
+       fill = "# of Pitches",
+       title = "Andrew Heaney Batted Balls on 4-Seam Fastballs") +
+  theme_classic()
+
